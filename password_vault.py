17239f015c1f4dce6dc97c469ec2b5e59766ca73
Superior-Snacks <joningiwork@gmail.com>
2025-09-13 20:46:43 +0000

    s

--- password_vault.py
+++ password_vault.py
@@ -105,7 +105,8 @@ def term_list(args):
     vault_data = read_vault(VAULT_PATH)
     entries, key, meta = open_vault(master, vault_data)
     if not entries:
-        print("foo, no entrys, that check out?... seems bad")
+        return print("foo, no entrys, that check out?... seems bad")
+
 
 def term_add(args):
     master = get_master()
