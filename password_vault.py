491c581560bb069b974348b1ba19f7fa87e79704
Superior-Snacks <joningiwork@gmail.com>
2025-09-15 19:39:37 +0000

    s

--- password_vault.py
+++ password_vault.py
@@ -47,6 +47,7 @@ def password_generator(length=20):
     rem = length - len(password)
     for i in range(rem):
         password += secrets.choice(all)
+        random.shuffle(password)
     return "".join(password)
 
 
