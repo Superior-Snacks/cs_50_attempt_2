d6d4da321e8351322fc9591f20a363cab49b49e5
Superior-Snacks <joningiwork@gmail.com>
2025-09-15 17:27:16 +0000

    s

--- password_vault.py
+++ password_vault.py
@@ -138,13 +138,21 @@ def term_delete(args):
     if not found:
         print("entry to delete not found")
         sys.exit(1)
-    
+        
+    new_entries = []
     if len(found) == 1:
+        for i in entries:
+            if i == found[0]:
+                continue
+            else:
+                new_entries.append(i)
         print(f"{found[0]["name"]} has been deleted")
     elif len(found) > 1:
         print("Multiple entries found, please be more precise")
         for i in found:
             print(json.dumps(i, indent=2))
+
+    save_to_vault(new_entries, key, meta)
         
 
 def term_show(args):
