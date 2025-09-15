390d3ff02aec3e74305c4ccf61c281f0289569d4
Superior-Snacks <joningiwork@gmail.com>
2025-09-15 17:45:38 +0000

    s

--- password_vault.py
+++ password_vault.py
@@ -86,10 +86,12 @@ def open_vault(master, vault_data):
     salt_b = vault_data["scram"]["salt_b64e"]
     iterations = int(vault_data["scram"]["iterations"])
     key = key_scram(master, b64d(salt_b), iterations)
-    entries = decrypt(key,
-        vault_data["cipher"]["iv"],
-        vault_data["cipher"]["ciphertext"]
-        )
+    try:
+        entries = decrypt(key,
+            vault_data["cipher"]["iv"],
+            vault_data["cipher"]["ciphertext"]
+            )
+    except Exception:
     return entries, key, vault_data
 
 
