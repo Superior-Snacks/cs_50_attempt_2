e5230860da867b5c4928e55368d5fd4453b7b4b2
Superior-Snacks <joningiwork@gmail.com>
2025-09-15 17:15:40 +0000

    s

--- password_vault.py
+++ password_vault.py
@@ -131,6 +131,16 @@ def term_delete(args):
     vault_data = read_vault(VAULT_PATH)
     entries, key, meta = open_vault(master, vault_data)
 
+    found = False
+    for i in entries:
+        if (args.id == i["id"]) or (args.name == i["name"]):
+            print(json.dumps(i, indent=2))
+            found = True
+    if not found:
+        print("entry not found")
+        sys.exit(1)
+
+
 def term_show(args):
     master = get_master()
     vault_data = read_vault(VAULT_PATH)
